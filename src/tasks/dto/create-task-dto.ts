076fa4c02a9c566652804ae0a059c8a026c5472b
lcsulak <lcsulak@navent.com>
2021-04-02 15:45:02 -0300

    add swagger documentation

--- src/tasks/dto/create-task-dto.ts
+++ src/tasks/dto/create-task-dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateTaskDto {
+  @ApiProperty({ type: String, description: 'titulo de la tarea' })
   title: string;
+
+  @ApiProperty({ type: String, description: 'descripcion de la tarea' })
   description: string;
+
+  @ApiProperty({ type: Boolean, description: 'is done or not' })
   done: boolean;
 }
