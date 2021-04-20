5fe0a99c3fa005826a3e84e8cb69ee0161f3c62d
lcsulak <lcsulak@navent.com>
2021-04-19 22:52:01 -0300

    delete unused console.log

--- src/gifs/gifs.service.ts
+++ src/gifs/gifs.service.ts
@@ -35,7 +35,6 @@ export class GifsService {
       .pipe(map((response) => response.data))
       .pipe(
         catchError((e) => {
-          console.log('asdasd', e.response.data);
           throw new HttpException(
             {
               ...e.response.data,
