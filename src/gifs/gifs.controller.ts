89e937bb95aa0f0d801641f549b84e21757ce32f
lcsulak <lcsulak@navent.com>
2021-04-19 22:49:31 -0300

    adding new approaches of mem cache

--- src/gifs/gifs.controller.ts
+++ src/gifs/gifs.controller.ts
@@ -14,17 +14,21 @@ export class GifsController {
   constructor(private gifsService: GifsService) {}
 
   /** Adding cache on this endpoint */
-  @CacheKey('GIFS')
+  // If I set the cache key here, for 20 secs (TTL) every search is going to return the same info.
+  //@CacheKey('GIFS')
   @CacheTTL(20)
-  @Get(':searchParam')
   @UseInterceptors(CacheInterceptor)
+  @Get(':searchParam')
   getGifs(@Param('searchParam') searchParam: string) {
-    console.log('entro');
+    console.log('searchings gifs by param: ', searchParam);
     return this.gifsService.getGifs(searchParam);
   }
 
+  @CacheTTL(60)
+  @UseInterceptors(CacheInterceptor)
   @Get('/specificGif/:gifId')
   getGif(@Param('gifId') gifId: string) {
+    console.log('retrieving info by Gif Id: ', gifId);
     return this.gifsService.getGif(gifId);
   }
 }
--- src/gifs/gifs.service.ts
+++ src/gifs/gifs.service.ts
@@ -1,5 +1,11 @@
-import { Injectable, HttpService, Inject, CACHE_MANAGER } from '@nestjs/common';
-import { map } from 'rxjs/operators';
+import {
+  Injectable,
+  HttpService,
+  Inject,
+  CACHE_MANAGER,
+  HttpException,
+} from '@nestjs/common';
+import { map, catchError } from 'rxjs/operators';
 import { Cache } from 'cache-manager';
 
 export const API_KEY = 'Sc9MBZzmvQXVCYeC4imY4vOcOeQty4Hc';
@@ -13,6 +19,7 @@ export class GifsService {
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
+  /**returns a list of gif that matches with the serchParam sent */
   getGifs(searchParam: string) {
     return this.http
       .get(
@@ -21,10 +28,24 @@ export class GifsService {
       .pipe(map((response) => response.data));
   }
 
+  /**returns info related an specific gif id */
   getGif(gifId: string) {
-    console.log({ gifId });
-    return this.http
+    const gifInfo = this.http
       .get(`${API_URL}/gifs/${gifId}?api_key=${API_KEY}`)
-      .pipe(map((response) => response.data));
+      .pipe(map((response) => response.data))
+      .pipe(
+        catchError((e) => {
+          console.log('asdasd', e.response.data);
+          throw new HttpException(
+            {
+              ...e.response.data,
+              mensaje_personalizado: `no se pudo encontrar la info para el Gif Id: ${gifId}`,
+            },
+            e.response.status,
+          );
+        }),
+      );
+
+    return gifInfo;
   }
 }
