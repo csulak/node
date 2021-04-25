b19f893f8f476eb28be78a4f4338f20237b8054a
lcsulak <lcsulak@navent.com>
2021-04-25 18:14:15 -0300

    IMPORTANT HOTFIX fixing the problem to read env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,19 +7,21 @@ import { TasksModule } from './tasks/tasks.module';
 import { GifsController } from './gifs/gifs.controller';
 import { GifsService } from './gifs/gifs.service';
 import { GifsModule } from './gifs/gifs.module';
-import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonsController } from './pokemons/pokemons.controller';
 import { PokemonsService } from './pokemons/pokemons.service';
 import { PokemonsModule } from './pokemons/pokemons.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     TasksModule,
-    AuthModule,
     MongooseModule.forRoot('mongodb://localhost/node-nest1'),
+    ConfigModule.forRoot(),
     GifsModule,
     PokemonsModule,
+    AuthModule,
   ],
   controllers: [
     AppController,
